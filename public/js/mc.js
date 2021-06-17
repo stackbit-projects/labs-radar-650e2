dfffeae515755d651d1a9e186615fe40dcefb9f4
Stackbit Code Editor <projects@stackbit.com>
2021-06-17 13:29:52 +0000

    mc.js: changed

--- public/js/mc.js
+++ public/js/mc.js
@@ -1,1 +1,1 @@
-<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/20d26e4e3b210d9ae8b4fdc7d/2ac319b685c39275d3729a21e.js")
+!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/20d26e4e3b210d9ae8b4fdc7d/2ac319b685c39275d3729a21e.js")
